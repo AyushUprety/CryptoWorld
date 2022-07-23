b9302cbec8e92d12893375ce96d5b4fa883c4a3e
AyushUprety <ayushuprety1@gmail.com>
2022-07-23 14:50:19 +0545

    Added provider and store configuration

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app";
 import "antd/dist/antd.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
