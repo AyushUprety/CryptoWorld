f940802cb7374a67ab4571cd65371ec8fc1861ee
AyushUprety <ayushuprety1@gmail.com>
2022-07-23 15:23:17 +0545

    Added a query for code to make request to coinApi

--- src/services/coinrankingapi.js
+++ src/services/coinrankingapi.js
@@ -1,3 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "be0b7c3582mshafb0cdeda175d83p1f8d6ejsn14223d0757f6",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoAPi", // what is this reducer for?
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    // this function instantly returns an object
+    getCoinExchange: builder.query({
+      query: () => "/exchanges",
+    }),
+  }),
+});
 // const options = {
 //   method: "GET",
 //   url: "https://coinranking1.p.rapidapi.com/coins",
