0f3644cc743f56668df4a212f8fc8c23022d7f0f
AyushUprety <ayushuprety1@gmail.com>
2022-07-24 18:20:12 +0545

    Added ant design format to display the data

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,11 +1,29 @@
 import { React, useState } from "react";
 import millify from "millify";
 import { useGetCryptosQuery } from "../services/coinrankingapi";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
 const Cryptocurrencies = () => {
   const { data, isLoading } = useGetCryptosQuery();
   const [coins, setCoins] = useState(data?.data?.coins);
   console.log(coins);
-  return <div>Cryptocurrencies</div>;
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              ></Card>
+            </Link>
+          </col>
+        ))}
+      </Row>
+    </>
+  );
 };
 export default Cryptocurrencies;
