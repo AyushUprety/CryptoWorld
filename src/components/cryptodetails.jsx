13a19ec5255e5b98341e6fc6083ad8a8c52364fd
AyushUprety <ayushuprety1@gmail.com>
2022-08-06 17:03:01 +0545

    Imported necessary packages in cryptodetails component

--- src/components/cryptodetails.jsx
+++ src/components/cryptodetails.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import Loader from "./Loader";
+import LineChart from "./LineChart";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const Cryptodetails = () => {
-  return <div>cryptodetails</div>;
+  return <div></div>;
 };
 
 export default Cryptodetails;
